996054265b09893ccb9a308b41f6e9febab74d03
tsukihiro94 <tsukishima.hiromu@gmail.com>
2020-08-02 17:36:15 +0900

    発送地域のactive_hash実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,14 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   
-  belongs_to_active_hash :category, :item_status
+  belongs_to_active_hash :category, :item_status, :delivery_fee, :region
   belongs_to :user
   has_one_attached :image
   has_one :order
   has_one_attached :image
 
   with_option presence: true do
-    validates :category_id, :item_status, numericality: {other_than: 1 }
+    validates :category_id, :item_status, :delivery_fee, :region, numericality: {other_than: 1 }
   end
   
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -42,8 +42,8 @@ RSpec.describe User, type: :model do
     end
 
     it "passwordが5文字以下であれば登録できないこと" do
-      @user.password = "12345"
-      @user.password_confirmation = "12345"
+      @user.password = "h2345"
+      @user.password_confirmation = "h2345"
       @user.valid?
       expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
     end
