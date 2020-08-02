baf1efd0ee5adc68984605f03ae9f0e242fb6518
tsukihiro94 <tsukishima.hiromu@gmail.com>
2020-08-02 13:27:45 +0900

    itemモデルのアソシエーション

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,7 +17,7 @@ before_action :move_to_index, only: :new
 private
 
 def item_params
-  params.require(:item).permit(:name, :info, :category_id, :item_status_id, :delivery_free_id, :region_id, :delivery_day_id, :price).merge(user_id: current_user.id)
+  params.require(:item).permit(:name, :info, :category_id, :item_status_id, :delivery_free_id, :region_id, :delivery_day_id, :price, :image).merge(user_id: current_user.id)
 end
 
   def move_to_index
