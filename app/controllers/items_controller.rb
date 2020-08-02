5fa9eccdff0f091c08ac5afbd49130b7d7c55433
tsukihiro94 <tsukishima.hiromu@gmail.com>
2020-08-02 13:23:20 +0900

    active_storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,10 +8,17 @@ before_action :move_to_index, only: :new
   
 
   def new
-    
+    @item = Item.new
   end
-
+  
+  def create
+    @item = Item.create(item_params)
+  end
 private
+
+def item_params
+  params.require(:item).permit(:name, :info, :category_id, :item_status_id, :delivery_free_id, :region_id, :delivery_day_id, :price).merge(user_id: current_user.id)
+end
 
   def move_to_index
     unless user_signed_in?
