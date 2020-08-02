653544f5b39144dda73f1e873e2a6ddba4655c8c
tsukihiro94 <tsukishima.hiromu@gmail.com>
2020-08-02 15:56:14 +0900

    item_statusのactive_hashを実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,14 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   
-  belongs_to_active_hash :category
+  belongs_to_active_hash :category, :item_status
   belongs_to :user
   has_one_attached :image
   has_one :order
   has_one_attached :image
 
-  validates :category_id, numericality: {other_than: 1 }
+  with_option presence: true do
+    validates :category_id, :item_status, numericality: {other_than: 1 }
+  end
+  
 end
