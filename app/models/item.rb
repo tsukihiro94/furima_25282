bb01f727926ec55836390c57d06c5541335086f3
tsukihiro94 <tsukishima.hiromu@gmail.com>
2020-08-02 15:31:57 +0900

    categoryのactive_hashの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  
+  belongs_to_active_hash :category
   belongs_to :user
   has_one_attached :image
   has_one :order
   has_one_attached :image
+
+  validates :category_id, numericality: {other_than: 1 }
 end
