c4c25a4f7cb453490eae9a4cc8ca3e8427c2e875
tsukihiro94 <tsukishima.hiromu@gmail.com>
2020-08-02 18:36:20 +0900

    belongs_to_active_hashの書き方を変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,17 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   
-  belongs_to_active_hash :category, :item_status, :delivery_fee, :region, :delivery_day
+  belongs_to_active_hash :category
+  belongs_to_active_hash :item_status
+  belongs_to_active_hash :delivery_fee
+  belongs_to_active_hash :region
+  belongs_to_active_hash :delivery_day
   belongs_to :user
   has_one_attached :image
   has_one :order
   has_one_attached :image
 
-  with_option presence: true do
+  with_options presence: true do
     validates :category_id, :item_status, :delivery_fee, :region, :delivery_day, numericality: {other_than: 1 }
   end
   
