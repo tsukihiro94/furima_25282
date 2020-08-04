c957f4fb775fcc1f35a052d196945336a32d90d3
tsukihiro94 <tsukishima.hiromu@gmail.com>
2020-08-04 18:46:53 +0900

    パスをprefixに変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,7 +27,7 @@ class ItemsController < ApplicationController
     
     def move_to_index
       unless user_signed_in?
-        redirect_to '/users/sign_in'
+        redirect_to new_user_session_path
       end
     end
 end
