f07ab31a463dbba9a2238982f2b7808853959d50
tsukihiro94 <tsukishima.hiromu@gmail.com>
2020-08-04 18:15:54 +0900

    itemモデルの単体テスト

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    name            {"きこりん"}
+    info            {"檜の模型"}
+    category_id     {"3"}
+    item_status_id  {"2"}
+    delivery_fee_id {"3"}
+    region_id       {"7"}
+    delivery_day_id {"2"}
+    price           {"5000"}
+    association :user
   end
 end
