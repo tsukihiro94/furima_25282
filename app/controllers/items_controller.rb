2cd0f4c8948f93712577b7c0094fd72cedbcb652
tsukihiro94 <tsukishima.hiromu@gmail.com>
2020-08-05 18:49:10 +0900

    エラーハンドリングの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   
   before_action :authenticate_user!, only: :new
-  before_action :set_item, only: [:show, :edit]
+  before_action :set_item, only: [:show, :edit, :update]
     def index
       @items = Item.all
     end
@@ -29,8 +29,12 @@ class ItemsController < ApplicationController
     end
 
     def update
-      item = Item.find(params[:id]).update(item_params)
-      redirect_to action: :show
+      @item.update(item_params)
+      if @item.save
+        redirect_to action: :show
+      else
+        render :edit
+      end
     end
   private
 
