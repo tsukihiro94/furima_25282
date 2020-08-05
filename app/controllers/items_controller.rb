9bc6a71f79121e12bf816a43c9a718b2b840cc81
tsukihiro94 <tsukishima.hiromu@gmail.com>
2020-08-05 13:44:45 +0900

    showアクションの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,6 +19,10 @@ class ItemsController < ApplicationController
         render :new
       end
     end
+
+    def show
+      @item = Item.find(params[:id])
+    end
   private
 
     def item_params
