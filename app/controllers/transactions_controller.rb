e3fddb882ebcff70d76ef1ee38623e537ad0f8bc
tsukihiro94 <tsukishima.hiromu@gmail.com>
2020-08-07 21:37:16 +0900

    購入ページで@itmeが表示されるようにitem_idをインスタンス変数に定義

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,6 +1,14 @@
 class TransactionsController < ApplicationController
+  before_action :set_item, only: :index
+
 
   def index
-    
+
   end
+
+private  
+    def set_item
+      @item = Item.find(params[:item_id])
+    end
+
 end
