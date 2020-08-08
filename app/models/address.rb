5301b25f6b6d0f5d4120c9a047635a1c5c2468f1
tsukihiro94 <tsukishima.hiromu@gmail.com>
2020-08-08 22:16:28 +0900

    addressのバリデーション

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,10 @@
 class Address < ApplicationRecord
   belongs_to :user
+
+
+  with_opition presence: true do
+    validates :postal_code, format: {with: /\A\d{11}\z/}
+    validates :region_id, :street_number, :buildimg_name,
+    validates :phone, format: {with: /\A\d{3}[-]\d{4}\z/ }
+  end
 end
