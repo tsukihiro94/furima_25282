d13ddb50808c27954069a648a70de66b1d7825b1
tsukihiro94 <tsukishima.hiromu@gmail.com>
2020-08-10 18:34:34 +0900

    インデントの修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,33 +2,28 @@ class OrdersController < ApplicationController
     before_action :set_item, only: [:index, :create, :pay_item]
     before_action :go_to_index, only: :index
   
-    def index
-      
+    def index 
      if current_user.id == @item.user_id 
        redirect_to items_path
      end
-
-      @order = OrderAddress.new
+     @order = OrderAddress.new
     end
-    
-  
+
     def create
       @order = OrderAddress.new(order_params)
-      # binding.pry
-      if @order.valid?
+     if @order.valid?
         pay_item
         @order.save
-        return redirect_to root_path
+       return redirect_to root_path
       else
         render :index
       end
     end
-    
-  
-  private  
-      def set_item
+
+ private  
+    def set_item
         @item = Item.find(params[:item_id])
-      end
+    end
   
     def order_params
        params.require(:order_address).permit(:price,:postal_code, :region_id, :city, :street_number, :building_name, :phone).merge(item_id: @item.id,user_id: current_user.id)
@@ -45,9 +40,8 @@ class OrdersController < ApplicationController
     end
 
     def go_to_index
-      if @item.order != nil 
+     if @item.order != nil 
         redirect_to items_path
-      end
+     end
     end
-
 end
